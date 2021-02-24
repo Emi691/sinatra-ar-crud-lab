a05f0b804d017ee382eedf4d9e8aee16dffa0df7
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-24 13:11:08 -0800

    commit tree 41de1a14d2cfb3107df9effb16889378086bd516

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,40 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article= Article.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    #binding.pry
+    @article=Article.find_by(id: params[:id])
+    @article.update({title: params[:title], content: params[:content]})
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.find_by(id: params[:id]).delete
+    redirect '/articles'
+  end
 end
